1429a81f77ee73535c20e3d1ace4c7886d2056fb
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-06-07 17:09:44 +0000

    1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,14 @@ class SessionsController < ApplicationController
     @user = User.find_by({ "email" => params["email"] })
     if @user
       if @user["password"] == params["password"]
+        flash["notice"] = "You've logged in."
         redirect_to "/places"
       else
+        flash["notice"] = "Unsuccessful login."
         redirect_to "/sessions/new"
       end
     else
+      flash["notice"] = "Unsuccessful login."
       redirect_to "/sessions/new"
     end
   end
