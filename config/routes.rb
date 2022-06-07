84e0e2aa3774058ecdce35d4ea3218ed9df2401b
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-06-07 17:54:48 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources "users"
 
   get "/logout", :controller => "sessions", :action => "destroy"
+  get "/login", :controller => "sessions", :action => "new"
 end
